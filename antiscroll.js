09f000f01b013b60e5eea4d4131cc0b7b2257d2d
mysamy <47317673+mysamy@users.noreply.github.com>
2025-02-04 16:55:04 +0100

    carousel

--- antiscroll.js
+++ antiscroll.js
@@ -1,12 +1,25 @@
 // Sauvegarder la position de défilement avant que la page ne soit fermée ou actualisée
-window.onbeforeunload = function () {
-  sessionStorage.setItem('scrollPosition', window.scrollY);
-};
+// window.onbeforeunload = function () {
+//   sessionStorage.setItem('scrollPosition', window.scrollY);
+// };
 
-// Restaurer la position de défilement lors du chargement de la page
-window.onload = function () {
-  const scrollPosition = sessionStorage.getItem('scrollPosition');
-  if (scrollPosition) {
-    window.scrollTo(0, scrollPosition);
-  }
-};
+// // Restaurer la position de défilement lors du chargement de la page
+// window.onload = function () {
+//   const scrollPosition = sessionStorage.getItem('scrollPosition');
+//   if (scrollPosition) {
+//     window.scrollTo(0, scrollPosition);
+//   }
+// };
+const wrapper = document.querySelector('.carousel__container');
+const items = document.querySelectorAll('.carousel__card');
+let index = 0;
+
+function scrollToNext() {
+  index = (index + 1) % items.length;
+  wrapper.scrollTo({
+    left: items[index].offsetLeft - (wrapper.offsetWidth - items[index].offsetWidth) / 2,
+    behavior: "smooth"
+  });
+}
+
+setInterval(scrollToNext, 5000); // Change de carte toutes les 5 secondes
